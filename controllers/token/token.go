57a92de7e919f780aee76760f1a85d6b9f34a024
kucherenkovova <kucherenkovova@gmail.com>
2018-12-06 18:04:27 +0200

    working gin handlers

--- controllers/token/token.go
+++ controllers/token/token.go
@@ -2,52 +2,37 @@ package token
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/getupandgo/snooper-wooper/validators"
+	"github.com/getupandgo/snooper-wooper/dao"
+	"github.com/getupandgo/snooper-wooper/models"
 	"github.com/gin-gonic/gin"
 )
 
-func ParseText(c *gin.Context) {
-	jsonBody := validators.Token{}
+const defaultLimit = "10"
 
-	if err := c.ShouldBindJSON(&jsonBody); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		//todo: log stuff
-		return
-	}
-	// todo: we must find a nice way to convert from validator to model here
-	//saved, err := db.SaveToken(jsonBody)
-	//if saved.Count == newToken.Count {
-	//	w.WriteHeader(http.StatusCreated)
-	//} else {
-	//	// todo: change to OK when we return body
-	//	w.WriteHeader(http.StatusNoContent)
-	//}
-	////todo: return the created/updated entity in body
-	//c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+type TokenController struct {
+	tokens dao.TokensDao
 }
 
-func GetTopTokens(c *gin.Context) {
-	//	// todo:
-	//	// it's a common scenario to support pagination with limit & offset
-	//	// also would be great to have total count in the response body
-	//	// fixme:
-	//	// 2. what if limit is missing?
-	params := validators.GetTopTokens{}
+func New(dao dao.TokensDao) TokenController {
+	return TokenController{dao}
+}
 
-	if err := c.ShouldBindQuery(&params); err != nil {
+func (ctrl TokenController) GetTopTokens(c *gin.Context) {
+	limit, _ := strconv.ParseUint(c.DefaultQuery("limit", defaultLimit), 10, 64)
+	tokens, _ := ctrl.tokens.GetTokens(limit)
+	c.JSON(http.StatusOK, tokens)
+}
+
+func (ctrl TokenController) UpsertToken(c *gin.Context) {
+	token := &models.Token{}
+	if err := c.ShouldBindJSON(token); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		//todo: log stuff
-		return
 	}
-
-	//	tokens, err := dao.GetTokens(limitNum); err != nil {
-	//		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	//	} else {
-	//		// todo:
-	//		// would be great if we had serialize function(or method)
-	//		// in order to hide the exact implementation
-	//		// so it will be possible to change serialization strategies
-	//		c.JSON(http.StatusBadRequest, gin.H{"tokens": tokens})
-	//	}
+	t, err := ctrl.tokens.SaveToken(token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+	}
+	c.JSON(http.StatusOK, t)
 }
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 
+	"github.com/getupandgo/snooper-wooper/controllers/token"
 	"github.com/getupandgo/snooper-wooper/dao"
 	"github.com/getupandgo/snooper-wooper/models"
 )
@@ -17,9 +18,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to init database with error %+v", err)
 	}
-	router := InitRouter(&ctx{tokens: dao.NewTokensDao(db)})
-	// fixme: handle error here (e.g. EADDRINUSE)
-	_ = http.ListenAndServe(":8000", router)
+	tokensCtrl := token.New(dao.NewTokensDao(db))
+	r := gin.New()
+	tokensRouter := r.Group("/tokens")
+	tokensRouter.GET("", tokensCtrl.GetTopTokens)
+	tokensRouter.POST("", tokensCtrl.UpsertToken)
+	//fixme: handle error here (e.g. EADDRINUSE)
+	_ = r.Run(":8000")
 }
 
 func initDB() (*gorm.DB, error) {
