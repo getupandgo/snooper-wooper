1509d9b6e7607607a1aa870a6190161d6e4c1597
kucherenkovova <kucherenkovova@gmail.com>
2018-12-12 00:50:17 +0200

    - added go mod - added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.11.2 AS builder
+
+COPY go.mod go.sum /src/
+WORKDIR /src
+
+RUN GO111MODULE=on go mod download
+# Build all binaries, with -getmode=local, this will _not_ fetch packages over the network.
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -o app
+
+# Runtime image
+FROM alpine:3.8 as base
+COPY --from=builder /src/app /bin/app
+ENTRYPOINT ["/bin/app"]
--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 }
 
 func initDB() (*gorm.DB, error) {
-	connectionString := fmt.Sprintf("host=%s port=%s user=postgres dbname=postgres password=postgres sslmode=disable", "localhost", "5432")
+	connectionString := fmt.Sprintf("host=%s port=%s user=postgres dbname=postgres password=postgres sslmode=disable", "postgres-snoop", "5432")
 	db, err := gorm.Open("postgres", connectionString)
 	if err != nil {
 		return nil, err
